fd5fadfa3bc7e5ea97f7fcd1cee0060afdfb148b
Дмитрий Сердюков <dimaserd84@gmail.com>
2020-02-02 22:00:48 +0300

    init

--- CrocoChat/CrocoChat/CrocoChat.Simple/Startup.cs
+++ CrocoChat/CrocoChat/CrocoChat.Simple/Startup.cs
@@ -111,6 +111,17 @@ namespace CrocoChat.Simple
                 app.UseDeveloperExceptionPage();
             }
 
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
